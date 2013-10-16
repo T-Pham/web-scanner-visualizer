ff105aec07babae438172a00221c87bd0428a6dc
Thanh Pham <Thanh.Pham@eastagile.com>
2013-10-16 18:45:18 +0700

    Refactor MyProject file: grouping related tasks into functions

--- MyProject.cpp
+++ MyProject.cpp
@@ -3,24 +3,38 @@
 #include "./src/database_handler.hpp"
 #include <iostream>
 
+// Functions
+void start_database();
+void finalize_database();
+
+void parse_wapiti();
+
+// Variables
+database_handler* db;
+
+// main
 int main()
 {
-	// std::cout << "0\n";
-
-	database_handler* db = new database_handler();
-	
-	// std::cout << "1\n";
-	db->open_database();
-	// std::cout << "2\n";
-	db->initialize_database();
-
-	wapiti_tree_parse("testphp.vulnweb.com.xml", db);
-
-	wapiti_parse("vulnerabilities.xml", db);
-
-	db->close_database();
-
-	delete db;
-
+  start_database();
+	parse_wapiti();
+  finalize_database();
 	return 1;
+}
+
+// Database functions
+void start_database() {
+	db = new database_handler();
+	db->open_database();
+	db->initialize_database();
+}
+
+void finalize_database() {
+  db->close_database();
+	delete db;
+}
+
+// Parsers
+void parse_wapiti() {
+  wapiti_tree_parse("testphp.vulnweb.com.xml", db);
+	wapiti_parse("vulnerabilities.xml", db);
 }
