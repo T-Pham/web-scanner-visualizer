2d0d75ec87d97c6a8bb02d9461e23832ce6fcbc5
Phuc Ly <lytienphuc0810@gmail.com>
2013-11-13 15:51:12 +0700

    add w3af parser to json_creator

--- src/json_creator.cpp
+++ src/json_creator.cpp
@@ -3,53 +3,11 @@
 #include "helper.hpp"
 #include "database_handler.hpp"
 #include "wapiti_parser.hpp"
+#include "w3af_parser.hpp"
 #include "arachni_parser.hpp"
 #include <fstream>
 #include <sstream>
 #include <string.h>
-
-std::string* get_node_info(int current_id, const char* error_type, database_handler* db_handler) {
-	std::string* str = new std::string();
-	std::stringstream ss;
-	// open
-	*str += "\"total\":";
-	int total = number_of_error(current_id , error_type, db_handler);
-	ss << total;
-	*str += ss.str();
-	ss.str("");
-
-	*str += ", \"arachni\":";
-	int wapiti = number_of_error_by_tool(current_id, ARACHNI_APP_NAME, error_type, db_handler);
-	ss << wapiti;
-	*str += ss.str();
-	ss.str("");
-
-	*str += ", \"wapiti\":";
-	int arachni = number_of_error_by_tool(current_id, WAPITI_APP_NAME, error_type, db_handler);
-	ss << arachni;
-	*str += ss.str();
-	ss.str("");
-
-	*str += ", \"pie\": [";
-
-	if(total) {
-		ss << wapiti*100/total;
-		*str += ss.str();
-		ss.str("");
-		*str += ", ";
-		ss << arachni*100/total;
-		*str += ss.str();
-		*str += ", 0";
-	}
-	else {
-		*str += "0, 0, 0, 100";
-	}
-
-	// close
-	*str += "]";
-
-	return str;
-}
 
 // load data from sqlite and create json
 void create_json() {
@@ -69,20 +27,75 @@ void create_json() {
 	delete db_handler;
 }
 
+std::string* get_node_info(int current_id, const char* error_type, database_handler* db_handler) {
+	std::string* str = new std::string();
+	std::stringstream ss;
+	// open
+	*str += "total:";
+	int total = number_of_error(current_id, error_type, db_handler);
+	ss << total;
+	*str += ss.str();
+	ss.str("");
+
+	*str += ", arachni: ";
+	int wapiti = number_of_error_by_tool(current_id, ARACHNI_APP_NAME, error_type, db_handler);
+	ss << wapiti;
+	*str += ss.str();
+	ss.str("");
+
+	*str += ", w3af: ";
+	int w3af = number_of_error_by_tool(current_id, W3AF_APP_NAME, error_type, db_handler);
+	ss << w3af;
+	*str += ss.str();
+	ss.str("");
+
+	*str += ", wapiti: ";
+	int arachni = number_of_error_by_tool(current_id, WAPITI_APP_NAME, error_type, db_handler);
+	ss << arachni;
+	*str += ss.str();
+	ss.str("");
+
+	*str += ", pie: [";
+
+	if (total) {
+		ss << wapiti * 100 / total;
+		*str += ss.str();
+		ss.str("");
+		*str += ", ";
+
+		ss << w3af * 100 / total;
+		*str += ss.str();
+		ss.str("");
+		*str += ", ";
+
+		ss << arachni * 100 / total;
+		*str += ss.str();
+		*str += ", 0";
+	}
+	else {
+		*str += "0, 0, 0, 100";
+	}
+
+	// close
+	*str += "]";
+
+	return str;
+}
+
 void create_graph_json(std::ofstream* file, database_handler* db_handler) {
 	std::string str1;
 	std::string str2;
 
 	sqlite3_stmt* sqlite_stmt;
-	if(sqlite3_prepare(db_handler->db, "SELECT * FROM URLS", -1, &sqlite_stmt, 0) == SQLITE_OK) {
-		while(sqlite3_step(sqlite_stmt) == SQLITE_ROW) {
+	if (sqlite3_prepare(db_handler->db, "SELECT * FROM URLS", -1, &sqlite_stmt, 0) == SQLITE_OK) {
+		while (sqlite3_step(sqlite_stmt) == SQLITE_ROW) {
 			std::stringstream ss;
 			int current_id = (int)sqlite3_column_int(sqlite_stmt, 0);
 			char* url = (char*)sqlite3_column_text(sqlite_stmt, 1);
 			char* method = (char*)sqlite3_column_text(sqlite_stmt, 2);
 			int parent_id = (int)sqlite3_column_int(sqlite_stmt, 3);
 
-			if(!str1.empty()) {
+			if (!str1.empty()) {
 				str1 += ",";
 			}
 			str1 += "{node: \"";
@@ -108,14 +121,14 @@ void create_graph_json(std::ofstream* file, database_handler* db_handler) {
 
 			str1 += "}}";
 
-			if(parent_id > 0) {
-				if(!str2.empty()) {
+			if (parent_id > 0) {
+				if (!str2.empty()) {
 					str2 += ",";
 				}
 				str2 += "{source: ";
 				ss << (parent_id - 1);
 				str2 += ss.str();
-				str2 +=  ", target: ";
+				str2 += ", target: ";
 				ss.str("");
 				ss << (current_id - 1);
 				str2 += ss.str();
@@ -139,11 +152,11 @@ std::string* get_params(const char* error_id, database_handler* db_handler) {
 	select_param_stmt += ";";
 	std::string* params = new std::string();
 
-	if(sqlite3_prepare(db_handler->db, select_param_stmt.c_str() , -1, &sqlite_stmt, 0) == SQLITE_OK) {
-		while(sqlite3_step(sqlite_stmt) == SQLITE_ROW) {
-			const char* param = (const char*) sqlite3_column_text(sqlite_stmt, 0);
+	if (sqlite3_prepare(db_handler->db, select_param_stmt.c_str(), -1, &sqlite_stmt, 0) == SQLITE_OK) {
+		while (sqlite3_step(sqlite_stmt) == SQLITE_ROW) {
+			const char* param = (const char*)sqlite3_column_text(sqlite_stmt, 0);
 
-			if(!params->empty()) {
+			if (!params->empty()) {
 				*params += ", ";
 			}
 			*params += "\"";
@@ -172,14 +185,14 @@ string_set* get_url_info(const int current_id, const char* tool_name, database_h
 	std::string* xss_injection_value = new std::string();
 
 
-	if(sqlite3_prepare(db_handler->db, select_error_stmt.c_str(), -1, &sqlite_stmt, 0) == SQLITE_OK) {
-		while(sqlite3_step(sqlite_stmt) == SQLITE_ROW) {
-			const char* error_id = (const char*) sqlite3_column_text(sqlite_stmt, 0);
-			const char* error_type = (const char*) sqlite3_column_text(sqlite_stmt, 1);
-			const char* injection_value = (const char*) sqlite3_column_text(sqlite_stmt, 2);
+	if (sqlite3_prepare(db_handler->db, select_error_stmt.c_str(), -1, &sqlite_stmt, 0) == SQLITE_OK) {
+		while (sqlite3_step(sqlite_stmt) == SQLITE_ROW) {
+			const char* error_id = (const char*)sqlite3_column_text(sqlite_stmt, 0);
+			const char* error_type = (const char*)sqlite3_column_text(sqlite_stmt, 1);
+			const char* injection_value = (const char*)sqlite3_column_text(sqlite_stmt, 2);
 
-			if(strcmp(error_type, "SQL Injection") == 0) {
-				if(!sqli_injection_value->empty()){
+			if (strcmp(error_type, "SQL Injection") == 0) {
+				if (!sqli_injection_value->empty()){
 					*sqli_injection_value += ", ";
 				}
 				*sqli_injection_value += "{injection_value: \"";
@@ -193,8 +206,8 @@ string_set* get_url_info(const int current_id, const char* tool_name, database_h
 				*sqli_injection_value += "]}";
 			}
 			else {
-				if(strcmp(error_type, "Cross Site Scripting") == 0) {
-					if(!xss_injection_value->empty()){
+				if (strcmp(error_type, "Cross Site Scripting") == 0) {
+					if (!xss_injection_value->empty()){
 						*xss_injection_value += ", ";
 					}
 					*xss_injection_value += "{injection_value: \"";
@@ -244,12 +257,12 @@ void create_params_json(std::ofstream* file, database_handler* db_handler) {
 
 	sqlite3_stmt* sqlite_stmt;
 
-	if(sqlite3_prepare(db_handler->db, "SELECT * FROM URLS", -1, &sqlite_stmt, 0) == SQLITE_OK) {
-		while(sqlite3_step(sqlite_stmt) == SQLITE_ROW) {
+	if (sqlite3_prepare(db_handler->db, "SELECT * FROM URLS", -1, &sqlite_stmt, 0) == SQLITE_OK) {
+		while (sqlite3_step(sqlite_stmt) == SQLITE_ROW) {
 			int current_id = (int)sqlite3_column_int(sqlite_stmt, 0);
 			char* url = (char*)sqlite3_column_text(sqlite_stmt, 1);
 			char* method = (char*)sqlite3_column_text(sqlite_stmt, 2);
-			if(!str.empty()) {
+			if (!str.empty()) {
 				str += ", ";
 			}
 
@@ -261,6 +274,14 @@ void create_params_json(std::ofstream* file, database_handler* db_handler) {
 
 			set = get_url_info(current_id, ARACHNI_APP_NAME, db_handler);
 			str += "arachni: ";
+			result = join_strings(set);
+			str += result->c_str();
+			delete result;
+			delete set;
+			str += ", ";
+
+			set = get_url_info(current_id, W3AF_APP_NAME, db_handler);
+			str += "w3af: ";
 			result = join_strings(set);
 			str += result->c_str();
 			delete result;
